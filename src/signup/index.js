7d585091a75ac8ed0d6c465f2b49f603a0e19f30
liulvjian <634098330@qq.com>
2017-09-07 20:01:47 +0800

    signup view

--- src/signup/index.js
+++ src/signup/index.js
@@ -1,5 +1,80 @@
 import React, { Component } from 'react'
+import PropTypes from 'proptypes'
+import { reduxForm, Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-class Signup extends Component {}
+import signupRequest from './action'
 
-export default Signup  
+class Signup extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func,
+    signupRequest: PropTypes.func,
+    signup: PropTypes.shape({
+      requesting: PropTypes.bool,
+      successful: PropTypes.bool,
+      message: PropTypes.array,
+      errors: PropTypes.array,
+    })
+  }
+
+  submit = (values) => {
+    this.props.signupRequest(values)
+  }
+
+  render() {
+    const {
+      handleSubmit,
+      signup: {
+        requesting,
+        successful,
+        message,
+        errors,
+      }
+    } = this.props
+
+    return (
+      <div className="signup">
+        <form className="widget-forms" onSubmit={handleSubmit(this.submit)}>
+          <h1>Signup</h1>
+          <label htmlFor="email">Email</label>
+          <Field
+            name="email"
+            type="text"
+            id="email"
+            className="email"
+            label="Email"
+            component="input"
+          />
+          <label htmlFor="password">Password</label>
+          <Field
+            name="password"
+            type="password"
+            id="password"
+            className="password"
+            label="Password"
+            component="input"
+          />
+          <button action="submit">SIGNUP</button>
+        </form>
+        <div className="auth-messages">
+          {
+
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  signup: state.signup,
+})
+
+const connected = connect(mapStateToProps, { signupRequest })(Signup)
+
+const formed = reduxForm({
+  form: 'signup',
+})(connected)
+
+export default formed  
