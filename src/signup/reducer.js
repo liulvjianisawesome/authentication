c3daccfb596d52683f850ab0ae09aa48b6c89dcf
liulvjian <634098330@qq.com>
2017-09-12 17:05:54 +0800

    complete signup saga

--- src/signup/reducer.js
+++ src/signup/reducer.js
@@ -1,4 +1,4 @@
-import { SIGNUP_REQUESTING } from './constants'
+import { SIGNUP_REQUESTING, SIGNUP_SUCCESS, SIGNUP_ERROR } from './constants'
 
 const initialState = {
   requesting: false,
@@ -15,6 +15,28 @@ const reducer = function (state = initialState, action) {
         successful: false,
         messages: [{ body: 'Signing up...', time: new Date() }],
         errors: [],
+      }
+
+    case SIGNUP_SUCCESS:
+      return {
+        requesting: false,
+        successful: true,
+        messages: [{
+          body: `Successfully created account for ${action.response.email}`,
+          time: new Date(),
+        }],
+        errors: [],
+      }
+
+    case SIGNUP_ERROR:
+      return {
+        requesting: false,
+        successful: false,
+        messages: [],
+        errors: state.errors.concat([{
+          body: action.error.toString(),
+          time: new Date(),
+        }]),
       }
 
     default:
